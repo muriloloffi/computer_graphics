9425944237311df90402ba3d04e43117a3000386
Murilo L <murilo.loffi@edu.udesc.br>
2019-10-24 21:49:34 -0300

    cubo pronto

--- cubo_rotacao.cpp
+++ cubo_rotacao.cpp
@@ -1,7 +1,6 @@
 #include <cstdlib>
 #include <GL/glut.h>
 #include <GL/glut.h>
-#define PI 3.1415926535898
 
 
 void display()
@@ -10,107 +9,80 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0,1.0,1.0);
 	glOrtho(-2.0,2.0,-2.0,2.0,-2.0,1.0);
-	//GLfloat parametro = 1;
-	//glRotatef(45,1,1,0);
-	/*glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glFrustum(-1, 1, -1, 1, 0.0, 40.0);
-	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
-	glRotatef(45.0, 1.0, 1.0, 1.0);*/
-	GLfloat vertices[8][3] = {{0,0,0},{1,0,0},{1,1,0},{0,1,0},{0,0,1},{1,0,1},{1,1,1},{0,1,1}};
-	glBegin(GL_TRIANGLES);
+	glRotatef(30,0.5,0.5,0.5);
+
+	GLfloat vertices[8][3] = 
+	{
+		{0,0,0}, //0-front-bottom-left
+		{0,1,0}, //1-front-upper-left
+		{1,1,0}, //2-front-upper-right
+		{1,0,0}, //3-front-bottom-right
+		{0,0,1}, //4-back-bottom-left
+		{0,1,1}, //5-back-upper-left
+		{1,1,1}, //6-back-upper-right
+		{1,0,1}  //7-back-bottom-right
+	};
+	glBegin(GL_TRIANGLES); //face frente - triangulo 1
+		//glColor3f(1.0,0,0);
 		glVertex3fv(vertices[0]);
-		glVertex3fv(vertices[3]);
+		glVertex3fv(vertices[1]);
 		glVertex3fv(vertices[2]);
 	glEnd();
-	glBegin(GL_TRIANGLES);
+	glBegin(GL_TRIANGLES); //face frente - triangulo 2
 		glVertex3fv(vertices[2]);
+		glVertex3fv(vertices[3]);
+		glVertex3fv(vertices[0]);
+	glEnd();	
+	glBegin(GL_TRIANGLES); //face topo - triangulo 3
+		glVertex3fv(vertices[1]);
+		glVertex3fv(vertices[5]);
+		glVertex3fv(vertices[6]);
+	glEnd();
+	glBegin(GL_TRIANGLES); // face topo - triangulo 4
+		glVertex3fv(vertices[6]);
+		glVertex3fv(vertices[2]);
+		glVertex3fv(vertices[1]);
+	glEnd();
+	glBegin(GL_TRIANGLES); // face esquerda - triangulo 5
+		glVertex3fv(vertices[0]);
+		glVertex3fv(vertices[4]);
+		glVertex3fv(vertices[5]);
+	glEnd();
+	glBegin(GL_TRIANGLES); // face esquerda - triangulo 6
+		glVertex3fv(vertices[5]);
 		glVertex3fv(vertices[1]);
 		glVertex3fv(vertices[0]);
 	glEnd();
-	/*glBegin(GL_TRIANGLES);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
+	glBegin(GL_TRIANGLES); // face direita - triangulo 7
+		glVertex3fv(vertices[3]);
+		glVertex3fv(vertices[7]);
+		glVertex3fv(vertices[6]);
 	glEnd();
-	glBegin(GL_TRIANGLES);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
+	glBegin(GL_TRIANGLES); // face direita - triangulo 8
+		glVertex3fv(vertices[6]);
+		glVertex3fv(vertices[2]);
+		glVertex3fv(vertices[3]);
 	glEnd();
-	glBegin(GL_TRIANGLES);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
+	glBegin(GL_TRIANGLES); // face trazeira - triangulo 9
+		glVertex3fv(vertices[7]);
+		glVertex3fv(vertices[4]);
+		glVertex3fv(vertices[5]);
 	glEnd();
-	glBegin(GL_TRIANGLES);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
+	glBegin(GL_TRIANGLES); // face trazeira - triangulo 10
+		glVertex3fv(vertices[5]);
+		glVertex3fv(vertices[6]);
+		glVertex3fv(vertices[7]);
 	glEnd();
-	glBegin(GL_TRIANGLES);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
+	glBegin(GL_TRIANGLES); // face base - triangulo 11
+		glVertex3fv(vertices[0]);
+		glVertex3fv(vertices[4]);
+		glVertex3fv(vertices[7]);
 	glEnd();
-	glBegin(GL_TRIANGLES);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
+	glBegin(GL_TRIANGLES); // face base - triangulo 12
+		glVertex3fv(vertices[7]);
+		glVertex3fv(vertices[3]);
+		glVertex3fv(vertices[0]);
 	glEnd();
-	glBegin(GL_TRIANGLES);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-	glEnd();
-	glBegin(GL_TRIANGLES);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-	glEnd();
-	glBegin(GL_TRIANGLES);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-	glEnd();
-	glBegin(GL_TRIANGLES);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-		glVertex3fv(vertices[i]);
-	glEnd();
-	*/			
-		
-				
-	/*
-		for (GLfloat i = 0; i < parametro; i+=0.01)
-		{
-			glColor3f(1.0,0.0,0.0);
-			GLfloat x = 0+i*(1-0);
-			GLfloat y = 0+i*(0-0);
-			//GLfloat angle = 2*PI*i/circle_points;
-			glVertex2f(x,y);
-		}
-		glBegin(GL_POINTS);
-		for (GLfloat i = 0; i < parametro; i+=0.01)
-		{
-			glColor3f(0.0,1.0,0.0);
-			GLfloat x = 0+i*(0-0);
-			GLfloat y = 0+i*(1-0);
-			//GLfloat angle = 2*PI*i/circle_points;
-			glVertex2f(x,y);
-		}
-		glBegin(GL_POINTS);
-		for (GLfloat i = 0; i < parametro; i+=0.01)
-		{
-			glColor3f(0.0,0.0,1.0);
-			GLfloat x = 0+i*(1-0);
-			GLfloat y = 0+i*(1-0);
-			//GLfloat angle = 2*PI*i/circle_points;
-			glVertex2f(x,y);
-		}
-	*/
-	//glEnd();
 	glFlush();
 }
 
