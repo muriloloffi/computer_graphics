60559ae55ef308b4f94347515f951f4e72be6969
Murilo L <murilo.loffi@edu.udesc.br>
2019-10-24 23:35:42 -0300

    Descriptions and comments

--- cubo_rotacao.cpp
+++ cubo_rotacao.cpp
@@ -9,76 +9,75 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0,1.0,1.0);
 	glOrtho(-2.0,2.0,-2.0,2.0,-2.0,1.0);
-	glRotatef(30,0.5,0.5,0.5);
+	glRotatef(45,0.5,0.5,0.5);
 
-	GLfloat vertices[8][3] = 
+	GLfloat vertices[8][3] = //Vector of vectors with cube vertices positions
 	{
 		{0,0,0}, //0-front-bottom-left
 		{0,1,0}, //1-front-upper-left
 		{1,1,0}, //2-front-upper-right
 		{1,0,0}, //3-front-bottom-right
-		{0,0,1}, //4-back-bottom-left
-		{0,1,1}, //5-back-upper-left
-		{1,1,1}, //6-back-upper-right
-		{1,0,1}  //7-back-bottom-right
+		{0,0,1}, //4-rear-bottom-left
+		{0,1,1}, //5-rear-upper-left
+		{1,1,1}, //6-rear-upper-right
+		{1,0,1}  //7-rear-bottom-right
 	};
-	glBegin(GL_TRIANGLES); //face frente - triangulo 1
-		//glColor3f(1.0,0,0);
+	glBegin(GL_TRIANGLES); //front face - triangle 1
 		glVertex3fv(vertices[0]);
 		glVertex3fv(vertices[1]);
 		glVertex3fv(vertices[2]);
 	glEnd();
-	glBegin(GL_TRIANGLES); //face frente - triangulo 2
+	glBegin(GL_TRIANGLES); //front face - triangle 2
 		glVertex3fv(vertices[2]);
 		glVertex3fv(vertices[3]);
 		glVertex3fv(vertices[0]);
 	glEnd();	
-	glBegin(GL_TRIANGLES); //face topo - triangulo 3
+	glBegin(GL_TRIANGLES); //top face - triangle 3
 		glVertex3fv(vertices[1]);
 		glVertex3fv(vertices[5]);
 		glVertex3fv(vertices[6]);
 	glEnd();
-	glBegin(GL_TRIANGLES); // face topo - triangulo 4
+	glBegin(GL_TRIANGLES); //top face - triangle 4
 		glVertex3fv(vertices[6]);
 		glVertex3fv(vertices[2]);
 		glVertex3fv(vertices[1]);
 	glEnd();
-	glBegin(GL_TRIANGLES); // face esquerda - triangulo 5
+	glBegin(GL_TRIANGLES); //left face - triangle 5
 		glVertex3fv(vertices[0]);
 		glVertex3fv(vertices[4]);
 		glVertex3fv(vertices[5]);
 	glEnd();
-	glBegin(GL_TRIANGLES); // face esquerda - triangulo 6
+	glBegin(GL_TRIANGLES); //left face - triangle 6
 		glVertex3fv(vertices[5]);
 		glVertex3fv(vertices[1]);
 		glVertex3fv(vertices[0]);
 	glEnd();
-	glBegin(GL_TRIANGLES); // face direita - triangulo 7
+	glBegin(GL_TRIANGLES); //right face - triangle 7
 		glVertex3fv(vertices[3]);
 		glVertex3fv(vertices[7]);
 		glVertex3fv(vertices[6]);
 	glEnd();
-	glBegin(GL_TRIANGLES); // face direita - triangulo 8
+	glBegin(GL_TRIANGLES); //right face - triangle 8
 		glVertex3fv(vertices[6]);
 		glVertex3fv(vertices[2]);
 		glVertex3fv(vertices[3]);
 	glEnd();
-	glBegin(GL_TRIANGLES); // face trazeira - triangulo 9
+	glBegin(GL_TRIANGLES); //rear face - triangle 9
 		glVertex3fv(vertices[7]);
 		glVertex3fv(vertices[4]);
 		glVertex3fv(vertices[5]);
 	glEnd();
-	glBegin(GL_TRIANGLES); // face trazeira - triangulo 10
+	glBegin(GL_TRIANGLES); //rear face - triangle 10
 		glVertex3fv(vertices[5]);
 		glVertex3fv(vertices[6]);
 		glVertex3fv(vertices[7]);
 	glEnd();
-	glBegin(GL_TRIANGLES); // face base - triangulo 11
+	glBegin(GL_TRIANGLES); //bottom face - triangle 11
 		glVertex3fv(vertices[0]);
 		glVertex3fv(vertices[4]);
 		glVertex3fv(vertices[7]);
 	glEnd();
-	glBegin(GL_TRIANGLES); // face base - triangulo 12
+	glBegin(GL_TRIANGLES); //bottom face - triangle 12
 		glVertex3fv(vertices[7]);
 		glVertex3fv(vertices[3]);
 		glVertex3fv(vertices[0]);
