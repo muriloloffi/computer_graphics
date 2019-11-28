47a999755ec1e8e1559abea6226b3dd56ced9503
Murilo Loffi <muriloloffi@gmail.com>
2019-11-28 18:34:30 -0300

    Floor and stars added

--- TrabalhoFinal/tieFighter.cpp
+++ TrabalhoFinal/tieFighter.cpp
@@ -11,56 +11,87 @@ void display()
 	glColor3f(0.5,0.5,0.5);
 	glMatrixMode(GL_PROJECTION);
 		glLoadIdentity(); // Load the Identity Matrix to reset our drawing locations
-		// glOrtho(-2.0,2.0,-2.0,2.0,0.0,20.0);
-		glFrustum(-8.0, 8.0, -8.0, 8.0, 0, 40.0);
+		// gluPerspective(60.0,1.0,0.0,21.5);
+		glFrustum(-1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 20.0f);
 	glMatrixMode(GL_MODELVIEW);
-	glViewport(0, 0, 700, 700);
+	// glViewport(0, 0, 700, 700);
 
-	GLUquadricObj *leftWingArm, *leftWing, *rightWingArm, *rightWing;
+	GLUquadricObj *leftWingArm, *leftWing, *rightWingArm, *rightWing, *fighterCore;
 
 	leftWingArm = gluNewQuadric();
 	leftWing = gluNewQuadric();
 	rightWingArm = gluNewQuadric();
 	rightWing = gluNewQuadric();
+	fighterCore = gluNewQuadric();
 
 
-	glRotatef(45.0f,1.0f,1.0f,0.0f);
+	glRotatef(27.0f,1.0f,1.0f,0.0f);
 	glScalef(0.5,0.5,0.5);
-
 
 	glPushMatrix();
 		glTranslatef    (0.95, 0.0, 0.0);
 		glRotatef	(90.0f, 0.0f, 1.0f, 0.0f);
-		// here *render* your cylinder (create and delete it in the other place. Not while rendering)
-		gluDisk(leftWing,0.0f,1.0f,16.0f,16); 
+		glRotatef 	(-30.0f, 0.0f, 0.0f, 1.0f); //Rotate disk edge to match original model
+		gluDisk(leftWing,0.0f,1.0f,6.0f,16); 
 	glPopMatrix();
 
 	glPushMatrix();
 		glTranslatef    (0.45, 0.0, 0.0);
 		glRotatef	(90.0f, 0.0f, 1.0f, 0.0f);
-		// here *render* your cylinder (create and delete it in the other place. Not while rendering)
-		gluCylinder(leftWingArm,0.1f,0.1f,0.5f,16,7); 
+		gluCylinder(leftWingArm,0.1f,0.1f,0.5f,32,1); //Cylinder for 'wing' arms
 	glPopMatrix();
 
 	glPushMatrix ();
        glTranslatef    (0, 0.0, 0.0);
 	   glRotatef	(90.0f,1.0f,0.0f,0.0f);
-       glutSolidSphere 	(0.5, 20, 20);
+       gluSphere 	(fighterCore,0.5, 32, 32);
     glPopMatrix ();
 
 	glPushMatrix();
 		glTranslatef    (-0.45, 0.0, 0.0);
 		glRotatef	(-90.0f, 0.0f, 1.0f, 0.0f);
-		// here *render* your cylinder (create and delete it in the other place. Not while rendering)
-		gluCylinder(rightWingArm,0.1f,0.1f,0.5f,16,7); 
+		gluCylinder(rightWingArm,0.1f,0.1f,0.5f,32,1); 
 	glPopMatrix();
 
 	glPushMatrix();
 		glTranslatef    (-0.95, 0.0, 0.0);
 		glRotatef	(-90.0f, 0.0f, 1.0f, 0.0f);
-		// here *render* your cylinder (create and delete it in the other place. Not while rendering)
-		gluDisk(rightWing,0.0f,1.0f,16.0f,16); 
+		glRotatef 	(-30.0f, 0.0f, 0.0f, 1.0f);
+		gluDisk(rightWing,0.0f,1.0f,6.0f,16); 
 	glPopMatrix();	
+
+
+	//Draw a floor
+	glPushMatrix();
+	for(float i = -50; i <= 50; i += 0.5)
+	{
+		glBegin(GL_POLYGON);
+			glColor3ub(150, 190, 150);						
+			glVertex3f(-50, -1.8, i);					
+			glVertex3f(50, -1.8, i);
+			glVertex3f(i, -1.8,-50);							
+			glVertex3f(i, -1.8, 50);
+		glEnd();
+	}
+	glPopMatrix();
+
+	//Draw some stars
+	glPointSize(2);
+	glPushMatrix();
+		glBegin(GL_POINTS);
+			glColor3f(1.0,1.0,1.0);
+			glVertex3f(0.0,-1.0,0.1);
+			glVertex3f(1.5,-0.9,0.4);
+			glVertex3f(-0.5,2.0,0.3);
+			glVertex3f(-1.0,1.8,0.2);
+			glVertex3f(1.0,2.0,0.6);
+			glVertex3f(-0.3,2.0,0.7);
+			glVertex3f(-1.4,1.7,1.5);
+			glVertex3f(-1.6,1.5,0.9);
+			glVertex3f(-0.9,1.6,0.9);
+			glVertex3f(-1.8,1.33,0.9);
+		glEnd();
+	glPopMatrix();
 
 	glFlush();
 }
@@ -90,7 +121,7 @@ int main(int argc, char *argv[])
 	// Create a window
 	glutCreateWindow("OpenGL Tie Fighter");
 	// Just draw the triangles edges:
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	// Register display callback
 	glutDisplayFunc(display);
 
