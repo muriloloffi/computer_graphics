cfde03e6d156d1b56ee224b2aed5656777d2d709
Murilo Loffi <muriloloffi@gmail.com>
2019-11-28 15:23:17 -0300

    Perspective and Scale defined

--- TrabalhoFinal/tieFighter.cpp
+++ TrabalhoFinal/tieFighter.cpp
@@ -8,10 +8,13 @@ void display()
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	//Set fill color (R,G,B):
-	glColor3f(1.0,1.0,1.0);
-	glOrtho(-8.0,16.0,-8.0,16.0,-10.0,8.0);
-	glFrustum(-4.0, 4.0, -4.0, 4.0, 1.5, 20.0);
- 
+	glColor3f(0.5,0.5,0.5);
+	glMatrixMode(GL_PROJECTION);
+		glLoadIdentity(); // Load the Identity Matrix to reset our drawing locations
+		// glOrtho(-2.0,2.0,-2.0,2.0,0.0,20.0);
+		glFrustum(-8.0, 8.0, -8.0, 8.0, 0, 40.0);
+	glMatrixMode(GL_MODELVIEW);
+	glViewport(0, 0, 700, 700);
 
 	GLUquadricObj *leftWingArm, *leftWing, *rightWingArm, *rightWing;
 
@@ -20,9 +23,10 @@ void display()
 	rightWingArm = gluNewQuadric();
 	rightWing = gluNewQuadric();
 
-	glLoadIdentity(); // Load the Identity Matrix to reset our drawing locations
 
 	glRotatef(45.0f,1.0f,1.0f,0.0f);
+	glScalef(0.5,0.5,0.5);
+
 
 	glPushMatrix();
 		glTranslatef    (0.95, 0.0, 0.0);
@@ -56,11 +60,20 @@ void display()
 		glRotatef	(-90.0f, 0.0f, 1.0f, 0.0f);
 		// here *render* your cylinder (create and delete it in the other place. Not while rendering)
 		gluDisk(rightWing,0.0f,1.0f,16.0f,16); 
-	glPopMatrix();
-	
+	glPopMatrix();	
 
 	glFlush();
 }
+
+const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 2.0f, 0.0f, 0.0f, 0.0f }; 
+ 
+const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f }; 
 
 void keyboard(unsigned char key, int x, int y)
 {
@@ -80,6 +93,21 @@ int main(int argc, char *argv[])
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	// Register display callback
 	glutDisplayFunc(display);
+
+	glEnable(GL_LIGHT0);
+	    glEnable(GL_NORMALIZE);
+	    glEnable(GL_COLOR_MATERIAL);
+	    glEnable(GL_LIGHTING); 
+	 
+	    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+	    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+	    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	    glLightfv(GL_LIGHT0, GL_POSITION, light_position); 
+	 
+	    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+	    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+	    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+	    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
 	// Register keyboard callback
 	glutKeyboardFunc(keyboard);
 	// Enter main event loop
